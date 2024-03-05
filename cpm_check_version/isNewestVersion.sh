6fb04e0023b444bf73b9f35ac60ad3ea8a741c23
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-03-05 22:07:50 +0100

    fix: add missing license information

--- cpm_check_version/isNewestVersion.sh
+++ cpm_check_version/isNewestVersion.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
+# SPDX-FileCopyrightText: 2006-2023, Knut Reinert & Freie Universität Berlin
+# SPDX-FileCopyrightText: 2016-2023, Knut Reinert & MPI für molekulare Genetik
+# SPDX-License-Identifier: CC0-1.0
 name=${1:-}
 path=${2:-}
 version=${3:-}
