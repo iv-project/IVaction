d5f91ec5625a8827cce39c458db6362f5d73c43d
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-03-12 08:29:48 +0100

    fix: more tolerant version checking

--- cpm_check_version/isNewestVersion.sh
+++ cpm_check_version/isNewestVersion.sh
@@ -2,6 +2,9 @@
 # SPDX-FileCopyrightText: 2006-2023, Knut Reinert & Freie Universität Berlin
 # SPDX-FileCopyrightText: 2016-2023, Knut Reinert & MPI für molekulare Genetik
 # SPDX-License-Identifier: CC0-1.0
+
+set -Eeuo pipefail
+
 name=${1:-}
 path=${2:-}
 version=${3:-}
@@ -16,11 +19,18 @@ then
 fi
 
 cd $path
+
+# Check if git repository
+if [ ! -e .git ]; then
+    exit 0
+fi
+
 git fetch origin 'refs/tags/*:refs/tags/*'
-newestVersion=$(git tag | cat - <(echo ${version}) | sed "s/^[vV]//" | sort -V -r | head -n 1)
+newestVersion=$(git tag | cat - <(echo ${version}) | sed "s/^[vV]//" | grep "^[0-9]" | grep -v '-'  | sort -V -r | head -n 1)
 
 if [ "${newestVersion}" == "${version}" ] \
-    || [ v"${newsetVersion}" == "${version}" ]; then
+    || [ v"${newestVersion}" == "${version}" ] \
+    || [ V"${newestVersion}" == "${version}" ]; then
     exit 0;
 fi
 
