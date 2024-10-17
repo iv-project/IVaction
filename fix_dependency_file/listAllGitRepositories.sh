0de676de722352fdcd65741413db592e71fa4066
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-10-17 14:25:25 +0200

    fix: listing of version number

--- fix_dependency_file/listAllGitRepositories.sh
+++ fix_dependency_file/listAllGitRepositories.sh
@@ -15,16 +15,10 @@ jq -r '
     | if has("github_repository") then
         (.git_repository = "https://github.com/\(.github_repository).git")
         end
-    | if has("git_tag") then
-        .real_version = .git_tag
-      elif has("version") then
-        .real_version = "v\(.version)"
-      end
     | if has("git_tag_ignore") then
         .git_tag_ignore |= (. | @tsv | gsub("\t"; "|"; "g"))
       end
-    | select(.real_version != null)
     | select(.git_repository != null)
-    | [.name, .real_version, .git_repository, .git_tag_ignore] | @tsv' \
+    | [.name, .version, .git_repository, .git_tag_ignore] | @tsv' \
 ${jsonpath}
 
