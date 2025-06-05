3923bc60862493024ee10a08702cb0c5b8f3b04b
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2025-06-05 19:16:32 +0200

    patch: demonstrate error on empty archive

---
+++ test-with_empty_archive/test.cpp
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Simon Gene Gottlieb
+// SPDX-License-Identifier: CC0-1.0
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World! " << "\n";
+}
---
+++ test-with_empty_archive/test3.cpp
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Simon Gene Gottlieb
+// SPDX-License-Identifier: CC0-1.0
+
+#include <cstdio>
+
+void ar_test3() {
+    printf("This is ar-test2.c\n");
+}
